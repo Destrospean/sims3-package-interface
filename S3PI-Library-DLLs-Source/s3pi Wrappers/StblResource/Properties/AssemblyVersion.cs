42ce2a585227775f15af73b470409612d003f493
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-07-24 10:08:26 -0500

    Fix build errors on non-Windows platforms

--- S3PI-Library-DLLs-Source/s3pi Wrappers/StblResource/Properties/AssemblyVersion.cs
+++ S3PI-Library-DLLs-Source/s3pi Wrappers/StblResource/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("1705.20.1719.*")]
+[assembly: AssemblyVersion("2607.24.1504.*")]
